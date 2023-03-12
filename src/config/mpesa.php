d688669812962acadb548c8204f855a87d97317c
Simon Siva <simonsiva13@gmail.com>
2023-03-12 09:36:44 +0300

    fix config

--- src/config/mpesa.php
+++ src/config/mpesa.php
@@ -36,17 +36,6 @@ return [
         'result_url' => getenv('MPESA_RESULT_URL'),
         'timeout_url' => getenv('MPESA_TIMEOUT_URL'),
         'initiator_name' => getenv('MPESA_INITIATOR_NAME'),
-        'default_command_id' => 'AccountBalance',
-        'security_credential' => getenv('MPESA_SECURITY_CREDENTIAL'),
-        'security_cert' => getenv('MPESA_SECURITY_CERT'),
-        'identifier_type' => getenv('MPESA_ACCOUNT_IDENTIFIER'),
-        
-    ],
-    'accsount' => [
-        'short_code' => getenv('MPESA_SHORTCODE'),
-        'result_url' => getenv('MPESA_RESULT_URL'),
-        'timeout_url' => getenv('MPESA_TIMEOUT_URL'),
-        'initiator_name' => getenv('MPESA_INITIATOR_NAME'),
         'security_credential' => getenv('MPESA_SECURITY_CREDENTIAL'),
         'security_cert' => getenv('MPESA_SECURITY_CERT'),
         'identifier_type' => getenv('MPESA_ACCOUNT_IDENTIFIER'),
