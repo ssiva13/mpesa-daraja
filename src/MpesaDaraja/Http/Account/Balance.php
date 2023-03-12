b840f0f4ee3a2228deb03f33ce9dde4ad1390274
Simon Siva <simonsiva13@gmail.com>
2023-03-12 08:36:56 +0300

    accont balance checks

---
+++ src/MpesaDaraja/Http/Account/Balance.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * Date 12/03/2023
+ *
+ * @author   Simon Siva <simonsiva13@gmail.com>
+ */
+
+namespace Ssiva\MpesaDaraja\Http\Account;
+
+use Ssiva\MpesaDaraja\Http\AbstractDarajaQuery;
+
+class Balance extends AbstractDarajaQuery
+{
+    protected string $endpoint = 'mpesa/accountbalance/v1/query';
+    
+    public function submitRequest(array $params = [], string $app = 'default')
+    {
+        // Make sure all the indexes are in Uppercases as shown in docs
+        $userParams = formatParams($params);
+        
+        $shortCode = configStore()->get('mpesa.account.short_code');
+        $resultCallback  = configStore()->get('mpesa.account.result_url');
+        $timeoutCallback  = configStore()->get('mpesa.account.timeout_url');
+        $initiator  = configStore()->get('mpesa.account.initiator_name');
+        $commandId  = configStore()->get('mpesa.account.default_command_id');
+        $pass = configStore()->get('mpesa.account.security_credential');
+        $identifierType = configStore()->get('mpesa.account.identifier_type');
+        
+        $configParams = [
+            'Initiator' => $initiator,
+            'SecurityCredential' => computeSecurityCredential($pass),
+            'IdentifierType' => $identifierType,
+            'CommandID' => $commandId,
+            'PartyA' => $shortCode,
+            'QueueTimeOutURL' => $timeoutCallback,
+            'ResultURL' => $resultCallback,
+        ];
+        // This gives precedence to params coming from user allowing them to override config params
+        $body = array_merge($configParams, $userParams);
+        $token = $this->bearer($app);
+        $response = $this->coreClient->makeRequest(
+            $this->endpoint,
+            'POST',
+            [
+                'json' => $body,
+                'headers' => [
+                    'Authorization' => 'Bearer ' . $token,
+                ]
+            ]
+        );
+        $contents = $response->getBody()->getContents();
+        return json_decode($contents);
+    }
+    
+}
