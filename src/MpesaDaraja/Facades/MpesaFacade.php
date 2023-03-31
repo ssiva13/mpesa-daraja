88c29609aa392c513700b8536d009c28d56a004e
Simon Siva <simonsiva13@gmail.com>
2023-04-01 02:52:17 +0300

    add laravel support

---
+++ src/MpesaDaraja/Facades/MpesaFacade.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Ssiva\MpesaDaraja\Facades;
+
+use Illuminate\Support\Facades\Facade;
+
+/**
+ * Date 01/04/2023
+ *
+ * @author   Simon Siva <simonsiva13@gmail.com>
+ */
+class MpesaFacade extends Facade
+{
+    /**
+     * Get the registered name of the component.
+     *
+     * @return string
+     */
+    protected static function getFacadeAccessor(): string
+    {
+        return 'Ssiva\MpesaDaraja\Mpesa';
+    }
+}
---
+++ src/MpesaDaraja/MpesaServiceProvider.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Ssiva\MpesaDaraja;
+
+use Illuminate\Support\ServiceProvider;
+
+/**
+ * Date 01/04/2023
+ *
+ * @author   Simon Siva <simonsiva13@gmail.com>
+ */
+class MpesaServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     */
+    public function boot()
+    {
+        $this->publishes([
+            __DIR__.'/../config/mpesa.php' => config_path('mpesa.php'),
+        ]);
+    }
+    
+    /**
+     * Register the application services.
+     */
+    public function register()
+    {
+        $this->app->bind('Ssiva\MpesaDaraja\Mpesa', function ($app) {
+            $config = $app['config']->get('mpesa');
+            return new Mpesa($config);
+        });
+    }
+}
