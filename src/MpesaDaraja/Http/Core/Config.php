4cb995fc3454dbd2975c80cec7d2dbcda8968e8c
Simon Siva <simonsiva13@gmail.com>
2023-04-14 11:58:35 +0300

    reformat and fix config path

--- src/MpesaDaraja/Http/Core/Config.php
+++ src/MpesaDaraja/Http/Core/Config.php
@@ -15,23 +15,29 @@ class Config implements ConfigurationStore, ArrayAccess
 {
     protected array $items = [];
     
-    public function __construct($conf = []){
+    public function __construct($conf = [])
+    {
         // Config that comes with the package
-        $configFile =  approot_path('config/mpesa.php');
+        $configFile = approot_path('config/mpesa.php');
         $defaultConfig = [];
-        if(\is_file($configFile)){
+        if (\is_file($configFile)) {
             $defaultConfig = require $configFile;
         }
-        $defaultConfig = array_merge($defaultConfig,$conf);
-        
+        $defaultConfig = array_merge($defaultConfig, $conf);
         // Config after user edits the config file copied by the system
-        $userConfig    =  __DIR__ . '/../../../../../../config/mpesa.php';
-        $custom        = [];
+        $userConfig = __DIR__.'/../../../../../../../config/mpesa.php';
+        $custom = [];
         if (\is_file($userConfig)) {
             $custom = require $userConfig;
         }
-        
         $this->items = array_merge($defaultConfig, $custom);
+    }
+    
+    public function prepend($key, $value)
+    {
+        $array = $this->get($key);
+        array_unshift($array, $value);
+        $this->set($key, $array);
     }
     
     /**
@@ -39,24 +45,17 @@ class Config implements ConfigurationStore, ArrayAccess
      */
     public function get($key, $default = null)
     {
-        $key = str_replace("mpesa.","",$key);
+        $key = str_replace("mpesa.", "", $key);
         $array = $this->items;
-        if (! static::accessible($array)) {
+        if (!static::accessible($array)) {
             return $this->value($default);
         }
-    
-        if (is_null($key)) {
-            return $array;
-        }
-    
         if (static::exists($array, $key)) {
             return $array[$key];
         }
-    
-        if (strpos($key, '.') === false) {
+        if (!str_contains($key, '.')) {
             return $array[$key] ?: $this->value($default);
         }
-    
         foreach (explode('.', $key) as $segment) {
             if (static::accessible($array) && static::exists($array, $segment)) {
                 $array = $array[$segment];
@@ -64,32 +63,35 @@ class Config implements ConfigurationStore, ArrayAccess
                 return $this->value($default);
             }
         }
-    
         return $array;
     }
     
-    public function prepend($key, $value)
-    {
-        $array = $this->get($key);
-        
-        array_unshift($array, $value);
-        
-        $this->set($key, $array);
-    }
-    
-    public function all()
-    {
-        return $this->items;
-    }
-    
-    public static function accessible($value)
+    public static function accessible($value): bool
     {
         return is_array($value) || $value instanceof ArrayAccess;
+    }
+    
+    public function value($value)
+    {
+        return $value instanceof Closure ? $value() : $value;
+    }
+    
+    public static function exists($array, $key): bool
+    {
+        if ($array instanceof ArrayAccess) {
+            return $array->offsetExists($key);
+        }
+        return array_key_exists($key, $array);
     }
     
     public function offsetExists($offset)
     {
         return $this->has($offset);
+    }
+    
+    public function all()
+    {
+        return $this->items;
     }
     
     public function offsetGet($offset)
@@ -105,17 +107,5 @@ class Config implements ConfigurationStore, ArrayAccess
     public function offsetUnset($offset)
     {
         $this->set($offset, null);
-    }
-    
-    public function value($value){
-        return $value instanceof Closure ? $value() : $value;
-    }
-    
-    public static function exists($array, $key): bool
-    {
-        if ($array instanceof ArrayAccess) {
-            return $array->offsetExists($key);
-        }
-        return array_key_exists($key, $array);
     }
 }
